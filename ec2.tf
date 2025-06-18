ac26713ce560f8b1e85cb03901ab30d346359cc7
Kani-shk <kanishkksharma91@gmail.com>
2025-06-19 00:55:25 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,68 @@
+#key pair (login)
+resource "aws_key_pair" deployer_key {
+    key_name = "Terra-key-ec2"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOj4QTdJHufOLSAdG0+O8+Pc25SRjY6C3azYhfv1v32q kanishk@kanishk"
+  
+}
+#vpc (security group)
+resource "aws_default_vpc" default{
+     tags = {
+    Name = "Default VPC"
+  }
+}
+resource "aws_security_group" sec_grp{
+    name = "automate sg"
+    description = "this will add a terraform generated security group"
+    vpc_id = aws_default_vpc.default.id #interpolation(extraction or inheritance of a specific terraform data block)
+    tags = {
+        name = "automate sg"
+    }
+    #inbound rules
+     ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "SSH open"
+     }
+     ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "HTTP open"
+     }
+     ingress {
+      from_port = 8000
+      to_port = 8000
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "notes app flask"
+     }
+    #outbound rules
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "access for all outbound"
+    }
+}
+#ec2 (instance)
+resource "aws_instance" "pro_instance" {
+  key_name = aws_key_pair.deployer_key.key_name
+  security_groups = [ aws_security_group.sec_grp.name ]
+  instance_type = "t2.micro"
+  ami = "ami-0cb91c7de36eed2cb" #ubuntu
+  
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+}
+  tags = {
+    name = "kanishk-instance"
+  }
+
+
+
+}
