9fef383707406720977d6ee9bf649cc021e6e2bb
Kani-shk <kanishkksharma91@gmail.com>
2025-06-20 17:43:33 +0530

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,35 @@
+#output "ec2_public_ip" {
+ # value = aws_instance.pro_instance[*].public_ip #asterisk for multiple instances mentioned using count meta argument
+
+#}
+
+#output "ec2_public_dns" {
+ # value = aws_instance.pro_instance[*].public_dns
+
+#}
+
+#output "ec2_private_ip" {
+ # value = aws_instance.pro_instance[*].private_ip
+
+#}
+
 output "ec2_public_ip" {
-    value = aws_instance.pro_instance.public_ip
+    value = [
+        for instance in aws_instance.pro_instance : instance.public_ip
+    ]
   
 }
 
 output "ec2_public_dns" {
-    value = aws_instance.pro_instance.public_dns
+    value = [
+        for instance in aws_instance.pro_instance : instance.public_dns
+    ]
   
 }
 
 output "ec2_private_ip" {
-    value = aws_instance.pro_instance.private_ip
+    value = [
+        for instance in aws_instance.pro_instance : instance.private_ip
+    ]
   
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,19 @@
 variable "ec2_instance_type" {
-    default = "t2.micro"
-    type = string
-  
+  default = "t2.micro"
+  type    = string
+
 }
-variable "ec2_root_storage_size" {
-    default = 15
-    type = number
-  
+variable "ec2_default_root_storage_size" {
+  default = 10
+  type    = number
+
 }
 variable "ec2_ami_id" {
-    default = "ami-0cb91c7de36eed2cb"
-    type = string
+  default = "ami-0cb91c7de36eed2cb"
+  type    = string
+}
+
+variable "env" {
+  default = "prd"
+  type = string
 }
